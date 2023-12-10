962ea086c63744035093e235a8a3d7c0bdee6bdf
bbeals6 <bbeals6@ivytech.edu>
2023-12-10 14:38:09 -0600

    JWT

--- app.js
+++ app.js
@@ -22,15 +22,3 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes)
-
-app.get('/set-cookies', (req, res) => {
-  res.cookie('userUser', false)
-  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true })
-  res.send('you got the cookies')
-})
-
-app.get('/read-cookies', (req, res) => {
-  const cookies = req.cookies
-  console.log(cookies)
-  res.send(cookies)
-})
