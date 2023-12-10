6f9cdbcb52ac93d9c46d88c0b1403448ef7fe8fb
bbeals6 <bbeals6@ivytech.edu>
2023-12-10 14:30:14 -0600

    forms and cookies

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const authRoutes = require('./routes/authRoutes')
+const cookieParser = require('cookie-parser')
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json())
+app.use(cookieParser())
 
 // view engine
 app.set('view engine', 'ejs');
@@ -20,3 +22,15 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes)
+
+app.get('/set-cookies', (req, res) => {
+  res.cookie('userUser', false)
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true })
+  res.send('you got the cookies')
+})
+
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies
+  console.log(cookies)
+  res.send(cookies)
+})
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const { isEmail } = require('validator')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -14,6 +15,18 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Please enter a password.'],
     minlength: [6, 'Minimum password length is 6 characters.'],
   },
+})
+
+userSchema.post('save', function (doc, next) {
+  console.log('new user saved', doc)
+
+  next();
+})
+
+userSchema.pre('save', async function () {
+  console.log('user about to be saved')
+  const salt = await bcrypt.genSalt()
+  this.password = await bcrypt.hash(this.password, salt)
 })
 
 const User = mongoose.model('user', userSchema)
